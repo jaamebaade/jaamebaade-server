2ecea8f038e0f0b4ce132fa93fead409481c1c49
Mohammad Khosravi <mohammad.kh@smartech.ir>
2024-05-24 21:54:50 +0330

    chore: add base

---
+++ src/main/kotlin/ir/jaamebaade/jaamebaade/JaamebaadeApplication.kt
@@ -0,0 +1,11 @@
+package ir.jaamebaade.jaamebaade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaamebaadeApplication
+
+fun main(args: Array<String>) {
+    runApplication<JaamebaadeApplication>(*args)
+}
---
+++ src/test/kotlin/ir/jaamebaade/jaamebaade/JaamebaadeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ir.jaamebaade.jaamebaade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaamebaadeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
