c986dfe634900867f1e89a6c0a4257af0461e981
Mohammad Khosravi <mohammad.kh@smartech.ir>
2024-10-07 23:17:56 +0330

    feat: add name parameter to poet list endpoint

--- src/main/kotlin/ir/jaamebaade/jaamebaade/repository/PoetRepository.kt
+++ src/main/kotlin/ir/jaamebaade/jaamebaade/repository/PoetRepository.kt
@@ -1,10 +1,14 @@
 package ir.jaamebaade.jaamebaade.repository
 
 import ir.jaamebaade.jaamebaade.model.Poet
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PoetRepository : PagingAndSortingRepository<Poet, Int> {
     fun findById(id: Int): Poet
+
+    fun findByNameContains(name: String, pageable: Pageable): Page<Poet>
 }
